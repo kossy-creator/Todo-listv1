c676197b2c170eb0f2d87cef15f7296eefb0be12
Kossy Ene <71001437+kossy-creator@users.noreply.github.com>
2020-12-13 21:14:24 +0000

    Add files via upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { TodoListComponent} from './components/todo-list/todo-list.component';
+
+const routes: Routes = [
+  {path: '', component: TodoListComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
